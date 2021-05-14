b554c2aee545614d53b376add10ac6b81e698a4f
elaig <80265352+elaig@users.noreply.github.com>
2021-05-14 11:09:23 +0100

    Update SpringbootDockerAwsEcsApplication.java

--- src/main/java/com/javatechie/aws/ecs/SpringbootDockerAwsEcsApplication.java
+++ src/main/java/com/javatechie/aws/ecs/SpringbootDockerAwsEcsApplication.java
@@ -19,7 +19,7 @@ public class SpringbootDockerAwsEcsApplication {
 
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-        return "Hi " + name + " Welcome to javatechie AWS ECS Example";
+        return "Hi " + name + " Welcome to vibha P1 AWS ECS Example";
     }
 
     public static void main(String[] args) {
